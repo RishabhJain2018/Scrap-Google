f169b28f8cd402f1c4c71fc2253c2d13c0252d50
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-17 22:00:52 +0530

    Cleaning Code

--- scripts/scrap.py
+++ scripts/scrap.py
@@ -1,22 +1,55 @@
 import requests
+import csv
+import thread
 from bs4 import BeautifulSoup
 
-words = ["google"]
+with open('words.csv', 'rb') as csvfile:
 
-url = "https://www.google.co.in/search"
-headers = {'Referer':'https://www.google.co.in/', 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
-payload = { 'q' : words[0], 'start' : '0' }
-response = requests.get(url, params=payload, headers=headers)
+	for line in csvfile.readlines():
+		words=line.split(',')
+		# print words
 
-soup = BeautifulSoup(response.content, 'html.parser')
+class scrapThread(threading.Thread):
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
 
-f=open('response.txt', 'a')
 
-h3tags = soup.find_all('h3', class_='r')
-for h3 in h3tags:
-    try:
-    	f.write( h3.text + "  \n ")
-    	print h3.text
-    except:
-    	continue
-f.close()
+	def run(self):
+		print "Starting " + self.name
+		threadLock.acquire()
+		scrap(self.name, self.counter, word)
+		threadLock.release()
+
+
+def scrap(threadname, counter, word):
+	# j=0
+	# while j < len(words):
+
+	url = "https://www.google.co.in/search"
+	headers = {'Referer':'https://www.google.co.in/', 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
+	payload = { 'q' : str(word), 'start' : '0' }
+	response = requests.get(url, params=payload, headers=headers)
+	# print "j : ", response
+	soup = BeautifulSoup(response.content, 'html.parser')
+# f=open('text.html', 'w')
+# f.write(soup.prettify().encode('utf-8'))
+# f.close()
+	f=open(str(files[j]) + ".txt", 'a')
+	h3tags = soup.find_all('h3', class_='r')
+	for h3 in h3tags:
+	    try:
+	    	print "enter try"
+	    	f.write( h3.text + "  \n ")
+	    except:
+	    	print "*********************************"
+	    	# print "Enter except : ", j
+	    	print "words ", word
+	    	print "*********************************"
+	    	continue
+	# j=j+1    	
+	# print " Completed : ", j
+	f.close()
+
